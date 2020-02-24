d6f9dc10001b180f36a6e6854cad5bc9c7d23bcc
DougMustard <mostertdouglas4@gmail.com>
2020-02-24 12:49:08 +0200

    Added working tests

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public static string Prefix(string input){
+            string length=Convert.ToString(input.Length);
+            int wordCount=0;
+            /*for(int i = 0;i<input.Length;i++){
+                if(input[i]==' '){
+                    wordCount++;
+                }
+            }*/
+            string[] splits=input.Split(' ');
+            wordCount=splits.Length;
+            for(int i =0;i<splits.Length;i++){
+                if(splits[i]==""){
+                    wordCount--;
+                }
+            }
+            string numWords= Convert.ToString(wordCount);
+
+            return length+","+numWords+":"+input;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Prefix("what    ...  did you say?? "));
         }
     }
 }
